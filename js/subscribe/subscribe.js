1171c00fd8067d60edff7ca116f0f3c0aa6101ab
WeeYoungSeok <dndrldzk@gmail.com>
2024-08-20 18:17:34 +0900

    feat: 이메일 유효성 검사

--- js/subscribe/subscribe.js
+++ js/subscribe/subscribe.js
@@ -1,10 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
+    function emailCheck(email_address){     
+        email_regex = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+\.[a-zA-Z]{2,4}$/i;
+        if(!email_regex.test(email_address)){ 
+            return false; 
+        }else{
+            return true;
+        }
+    }
+
     const subscribeButton = document.querySelector('.subscribe_button');
     const thankBox = document.querySelector('.thank_box');
     const okHoduButton = document.querySelector('.thank_button');
-  
+
     // Subscribe 버튼 클릭 시 .thank_box 나타나게 설정
     subscribeButton.addEventListener('click', function (event) {
+        const email = document.getElementById('email');
+        const emailValeu = email.value
+        if (!emailCheck(emailValeu)) {
+            alert("올바른 이메일을 입력해주세요.");
+            event.preventDefault(); // 기본 동작 방지
+            return; // 함수 종료
+        }
       event.preventDefault(); // 폼 제출을 막기 위해 기본 동작 방지
       thankBox.style.display = 'flex'; // .thank_box를 화면에 표시
     });
@@ -19,5 +35,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     okHoduButton.addEventListener('click', () => {
         thankBox.style.display = 'none';
+        event.preventDefault();
+        // 폼 제출을 하면 새로고침 되므로 새로고침을 하지 않고
+        // 값을 전달하는 방법은 비동기뿐인 것 같다.
+
+        // 새로고침을 원하지 않는다면 아래 코드 주석 풀기
+
     })
   });
