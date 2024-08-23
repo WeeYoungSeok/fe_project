909864aa9f2517ceb908a5f084101a383a7090cb
WeeYoungSeok <dndrldzk@gmail.com>
2024-08-23 16:42:43 +0900

    feat: 모바일 메뉴바 구현 및 z-index 수정

--- js/header/header.js
+++ js/header/header.js
@@ -5,3 +5,21 @@ window.onscroll = function () {
     window.location.href = "index.html";
   });
 };
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".m_navbar_box").addEventListener("click", () => {
+    document.querySelector(".navbar_box").classList.toggle("active");
+    document.querySelector(".m_overlay").classList.toggle("active");
+    document.body.style.overflow = "hidden";
+  });
+
+  let removeActive = () => {
+    document.querySelector(".navbar_box").classList.remove("active");
+    document.querySelector(".m_overlay").classList.remove("active");
+    document.body.style.overflow = "";
+  };
+
+  document
+    .querySelector(".m_arrow_rigth_img")
+    .addEventListener("click", removeActive);
+  document.querySelector(".m_overlay").addEventListener("click", removeActive);
+});
